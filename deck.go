d6c1997cd7b7057187df65dc98b2080b52f301cb
dev-by-gabriel <78721935+dev-by-gabriel@users.noreply.github.com>
2021-06-05 18:04:43 +0100

    Worked on main.go, deck.go

--- deck.go
+++ deck.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 
 // Create a new type of deck
 // Which is a slice of strings
@@ -26,4 +31,28 @@ func (d deck) print() {
 	for _, card := range d {
 		fmt.Println(card)
 	}
+}
+
+func deal(d deck, handSize int) (deck, deck) {
+	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := ioutil.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	ss := strings.Split(string(bs), ",")
+
+	return deck(ss)
 }
